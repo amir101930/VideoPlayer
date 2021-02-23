745356cde5d2d998f67111b90531ada3961d521f
MohammaJavadTD <mohammadjavadtoughyani@gmail.com>
2021-02-23 19:33:31 +0330

    add URL

--- app/src/main/java/com/mohammadjavadtd/myapplication/MainActivity.java
+++ app/src/main/java/com/mohammadjavadtd/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         MediaController mediaControllerMain = new MediaController(this);
         videoViewMain.setMediaController(mediaControllerMain);
         //getting url and convert to the Uri and set on videoViewWidget
-        Uri videoUri = Uri.parse("https://hajifirouz1.cdn.asset.aparat.com/aparat-video/3b11bee74f86abf3cc4bcb3d5a06e72930270542-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImIxYTE4YTIxOGIyNDIyZDM4Yjg4NTM1NjkxY2NhYzQ4IiwiZXhwIjoxNjE0MTAxMjIzLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.QyslZLhIaoHlBWPGNDYtZU9nZvlqJJZA_xT8DLnK-kY");
+        Uri videoUri = Uri.parse("https://hajifirouz1.cdn.asset.aparat.com/aparat-video/c45b7fd66deba6ca3dbd9f1fd94d246630270848-240p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjMxN2Y2ZmU3NGQzMGFkYTM2MTVkNzg2YWY5ZjcwNjFmIiwiZXhwIjoxNjE0MTEyNTE4LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.OveiEgSrRzXNO-NP5kYET1u5CC2DDPQL4uYtKT4aJjM");
         videoViewMain.setVideoURI(videoUri);
         //Start Streaming video
         videoViewMain.start();
